16aa83b0acb8a9db00c9609159dbc306a545e94f
Sri1031 <137263522+Sri1031@users.noreply.github.com>
2024-11-19 22:48:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,11 +32,9 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-             
                values ('""" + name_on_order + """')"""
-
+    
     #st.write(my_insert_stmt)
     #st.stop()
 
@@ -45,3 +43,9 @@ if ingredients_list:
     if time_to_insert:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothifroot nutrition information 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
